cecc287046080cef8b54708754d16bc5c2eb47c3
Marc Anderson <marcda495@gmail.com>
2025-03-27 16:11:15 -0400

    'na'

--- src/lib/components/budget/item-details.svelte
+++ src/lib/components/budget/item-details.svelte
@@ -10,7 +10,7 @@
   import Editor from "$lib/components/editor/editor.svelte";
   import FrequencyPicker from "./frequency-picker.svelte";
   import DollarInput from "./dollar-input.svelte";
-  import MuiltilineInput from "$lib/components/muiltiline-input.svelte";
+  import WrappedInput from "$lib/components/wrapped-input.svelte";
 
   let { categoryIndex, budgetItemIndex }: { categoryIndex: number; budgetItemIndex: number } =
     $props();
@@ -23,10 +23,10 @@
 
 <div class="p-4 pt-8 h-full w-full">
   <div class="pb-4 pr-2 justify-between items-end flex w-full h-min gap-2">
-    <MuiltilineInput bind:value={budgetItem.name} placeholder="Untitled" />
+    <WrappedInput bind:value={budgetItem.name} placeholder="Untitled" />
     <div>
       <DollarInput bind:value={budgetItem.amount} class="border-none  font-medium text-xl md:text-xl w-min justify-self-end"/>
-      <FrequencyPicker  bind:frequency={budgetItem.frequency} class="mt-1  justify-self-end" />
+      <FrequencyPicker bind:frequency={budgetItem.frequency} class="mt-1  justify-self-end" />
     </div>
   </div>
   <Editor />
--- src/lib/components/wrapped-input.svelte
+++ src/lib/components/wrapped-input.svelte
@@ -6,20 +6,25 @@
     class: className = "",
   }: { value?: string; placeholder?: string; class?: string } = $props();
 
-  let showPlaceholder = $derived(!value.trim());
+  let showPlaceholder = $derived(!(value.length > 0));
+
+  function oninput() {
+    value = value.replace(/\n/g, "");
+  }
 </script>
 
 <div
   bind:innerText={value}
   contenteditable="plaintext-only"
   aria-roledescription="textbox"
+  {oninput}
   {placeholder}
   spellcheck="false"
   class={cn(
-    "before:opacity-50 before:pointer-events-none",
     "ring-offset-background focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
-    "align-bottom h-min p-1 rounded-md font-medium text-2xl md:text-2xl text-medium",
-    showPlaceholder && "before:content-[attr(placeholder)]",
+    "px-3 py-2 h-min rounded-md font-medium text-2xl md:text-2xl text-medium",
+    showPlaceholder &&
+      "before:content-[attr(placeholder)] before:opacity-50 before:pointer-events-none",
     className
   )}
 ></div>
