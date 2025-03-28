e9c272aa3a1e8e98f4f23bac2b7f36fe078efb5c
Marc Anderson <marcda495@gmail.com>
2025-03-28 16:16:32 -0400

    'na'

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -105,7 +105,7 @@
     <!-- show drawer on mobile -->
     {@render budgetContent()}
     <Drawer.Root bind:open={drawerOpen} onClose={clearWindowSelection}>
-      <Drawer.Content class="h-[calc(100svh-max(env(keyboard-inset-height),0px)-7rem)]">
+      <Drawer.Content class="pb-20">
         {#if selectedCategory >= 0 && selectedBudgetItem >= 0}
           <ItemDetails categoryIndex={selectedCategory} budgetItemIndex={selectedBudgetItem} />
         {:else}
